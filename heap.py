4364b66f14278315c888c7ca73ffb71cb867e343
Shivam Singh <89941409+shiv-03@users.noreply.github.com>
2022-10-10 23:59:57 +0530

    heap.py

---
+++ heap.py
@@ -0,0 +1,7 @@
+import heapq as hq
+def heapsort(iterable):
+    h = []
+    for value in iterable:
+        hq.heappush(h, value)
+    return [hq.heappop(h) for i in range(len(h))]
+print(heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0]))
